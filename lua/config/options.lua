338258fe212ac8fc73d4efdbe038e5ac7c943053
crowforkotlin <crowforkotlin@gmail.com>
2026-04-18 23:00:18 +0800

    fix: scope guicursor override to Ghostty only

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if vim.env.TERM_PROGRAM == "Ghostty" then
+  vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+end
+
 if vim.fn.has("win32") == 1 then
   -- 指向 zsh 的路径，如果 zsh 不在 PATH 里，请写绝对路径
   -- 例如 "C:/msys64/usr/bin/zsh.exe"
